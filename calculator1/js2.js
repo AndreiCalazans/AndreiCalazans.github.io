d884695dda17c2f7ca5ecfc290763e34c76961e5
AndreiCalazans <andreixoc@hotmail.com>
2016-12-11 17:08:16 -0200

    more improvements

--- calculator1/js2.js
+++ calculator1/js2.js
@@ -1,8 +1,8 @@
 var answer ;
 function result(){
-  console.log(calcform.display.value);
+
   answer = eval(calcform.display.value);
-console.log(answer);
+
   calcform.display.value = calcform.display.value +" = " +answer;
 
 
@@ -10,17 +10,29 @@ console.log(answer);
 
 
 function displayvalue(n){
-  if (n == "÷" ){
+  console.log(calcform.display.value.length);
+  if (n == "÷"){
     n = "/";
   }else if ( n == "x"){
     n = "*";
   }
 
-  if (/[/*+-]/.test(n) && typeof(answer) == "number" ){
+  if(/[/*+-.]/.test(n) && /[/*+-.]/.test(calcform.display.value[calcform.display.value.length-1]) ){
+
+    var array = calcform.display.value.split("");
+    array[array.length-1] = n ;
+    calcform.display.value = array.join("");
+
+
+  }else if (/[/*+-]/.test(n) && typeof(answer) == "number" ){
+
     calcform.display.value = answer;
     answer = "";
+    calcform.display.value = calcform.display.value.concat(n);
+  }else {
+    calcform.display.value = calcform.display.value.concat(n);
   }
-  calcform.display.value = calcform.display.value.concat(n);
+
 }
 function clearEntry(){
   if(calcform.display.value.length = 1){
@@ -30,33 +42,30 @@ function clearEntry(){
   calcform.display.value = calcform.display.value.substr(0 , calcform.display.value.length - 1 );
   }
 
-
-
 }
 function allClear(){
   calcform.display.value = "";
   answer = "";
 
 }
-
 document.addEventListener("keydown", function(n){
+  if(/([0-9/*+-.])/.test(n.key[0])){
+    if (/[/*+-.]/.test(n.key) && /[/*+-.]/.test(calcform.display.value[calcform.display.value.length-1])){
+     var array = calcform.display.value.split("");
+     array[array.length-1] = n.key;
+     calcform.display.value = array.join("");
 
-  if(/[0-9/*+-]/.test(n.key)){
-    if (/[/*+-]/.test(n.key) && typeof(answer) == "number" ){
+    }else if (/[/*+-]/.test(n.key) && typeof(answer) == "number" ){
       calcform.display.value = answer;
       answer = "";
+      calcform.display.value = calcform.display.value.concat(n.key);
+    }else{
+      calcform.display.value = calcform.display.value.concat(n.key);
     }
-
-    calcform.display.value = calcform.display.value.concat(n.key);
-
   }
-
   if (n.keyCode == 13){
     result();
   }else if(n.which == 8){
     clearEntry();
   }
-
-
-
 })
